91769da5e32739cfff62978b861cc7ab96cb74cf
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-08 22:43:28 +0200

    updated folder structure to have frontend folder

--- build/handlers/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const users_1 = require("../models/users");
-const secret = process.env.TOKEN_SECRET;
-const store = new users_1.UserStore();
-const index = async (req, res) => {
-    const users = await store.index();
-    res.json(users);
-};
