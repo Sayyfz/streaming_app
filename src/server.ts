00d589acc76621bcbbc0d4670a1959a3f1a5f886
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-08 22:39:54 +0200

    all migrations created

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,20 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-// import user_routes from './handlers/users';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: express.Application = express();
-const address: string = '0.0.0.0:3000';
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!');
 });
 
-// user_routes(app)
-
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`);
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
