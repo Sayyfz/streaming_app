b3acc643779ff1f5a9f9bf99418be93c6c98f0b8
Seif Ashraf <seif_fire2@hotmail.com>
2023-02-09 22:07:10 +0200

    added create method on users model

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import query from "../helpers/query.helpers";
+import config from "../config";
 import db from "../db";
 import { throwError } from "../helpers/error.helpers";
 
@@ -20,6 +22,28 @@ export class UserStore {
       throwError(`Cannot get users ${err}`, 422);
     } finally {
       conn.release();
+    }
+  }
+
+  async create(user: User): Promise<User> {
+    const conn = await db.connect();
+
+    try {
+      const existSql = query.exist('users', 'email');
+      const existResult = await conn.query(existSql, [user.email])
+      if(existResult.rows.length) {
+        throw Error('email already exists')
+      }
+
+      const insertSql = query.insert('users', [user], ['*'])
+      const hash = bcrypt.hashSync(user.password + config.env('BCRYPT_PASSWORD'), config.env('SALT_ROUNDS'))
+      const result = await db.query(insertSql.sql, [user.email, hash])
+      return result.rows[0];
+
+    } catch (err) {
+      throwError(`Cannot create user ${err}`, 422)
+    } finally {
+      conn.release()
     }
   }
 }
